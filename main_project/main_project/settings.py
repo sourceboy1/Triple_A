3bd277312d12909c9f3378a7bc8f5ca465976bce
Akanni Oluwaseun <seunakanni417@gmail.com>
2025-08-13 14:21:44 +0100

    new codes

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-gy%qkzk$5a8idz4s$%6o-x$l$8tkhxd22yss^6_5o1m7os5$yx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tripleastechng.com']
 
 
 # Application definition
