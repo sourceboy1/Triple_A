02e21273d70d4bce7a1d83a69002c7cdeee3ffe0
Akanni Oluwaseun <seunakanni417@gmail.com>
2025-08-14 08:26:17 +0100

    Front end build

--- main_project/settings.py
+++ main_project/settings.py
@@ -95,7 +95,9 @@ LOGIN_REDIRECT_URL = '/'
 
 
 ROOT_URLCONF = 'main_project.urls'
-FRONTEND_DIR = os.path.join(BASE_DIR,  'frontend_build')
+
+
+FRONTEND_DIR = os.path.join(BASE_DIR,  'reat_project')
 
 TEMPLATES = [
     {
