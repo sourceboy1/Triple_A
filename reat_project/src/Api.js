cf98e61500917a51a68521c5d9c682a07eb7ee1b
Akanni Oluwaseun <seunakanni417@gmail.com>
2025-08-22 22:51:44 +0100

    Fix in TokenContext

--- reat_project/src/Api.js
+++ reat_project/src/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // api.js
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://tripleastechng.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://tripleastechng.com",
 });
 
 
