922f93afa8f8813ba45cd59c11a61bc6cb218dce
Akanni Oluwaseun <seunakanni417@gmail.com>
2025-08-29 15:38:05 +0100

    Maintance page

--- reat_project/src/components/Maintenance.js
+++ reat_project/src/components/Maintenance.js
@@ -7,16 +7,15 @@ const Maintenance = () => {
   return (
     <div className="maintenance-container">
       <h1>We’ll be back soon!</h1>
-      <img src={maintenanceGif} alt="Maintenance in progress" className="maintenance-gif" />
+      <img 
+        src={maintenanceGif} 
+        alt="Maintenance in progress" 
+        className="maintenance-gif" 
+      />
       <p>
         Sorry for the inconvenience, but we're performing some maintenance right now. 
         Please check back later.
       </p>
-
-      {/* 👇 Hidden Admin Login link */}
-      <div className="admin-login-link">
-        <a href="/admin">Admin Login</a>
-      </div>
     </div>
   );
 };
