241444eb63e8f4d8bda2dd19652e954674869a62
Akanni Oluwaseun <seunakanni417@gmail.com>
2026-05-21 16:06:51 +0100

    Updating Images Components

--- reat_project/src/components/Footer.js
+++ reat_project/src/components/Footer.js
@@ -104,7 +104,7 @@ const Footer = () => {
             {/* Bottom bar */}
             <div className="footer-bottom">
                 <p>© {new Date().getFullYear()} Triple A's Technology. All rights reserved.</p>
-                <p>2 Oba Akran, Ikeja, Lagos.</p>
+                <p>2 Oba Akran Avenuex , Ikeja, Lagos.</p>
             </div>
 
             <FooterSection />
